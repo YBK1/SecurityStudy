5d4e07db42c7c2bc634cbb5cd95a6e41abaf5c1b
YBK1 <kyb031700@naver.com>
2025-06-03 11:18:50 +0900

    Feat: SecurityConfig.java

---
+++ src/main/java/com/study/securitystudy/global/auth/jwt/LoginFilter.java
@@ -0,0 +1,4 @@
+package com.study.securitystudy.global.auth.jwt;
+
+public class LoginFilter {
+}
---
+++ src/main/java/com/study/securitystudy/global/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.study.securitystudy.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((auth) -> auth.disable());
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll()
+                        //.requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
